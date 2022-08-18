c048ff56fe14c6a072e13313b58706af8d235b23
azizul-orvee <orvee16@gmail.com>
2022-08-19 01:23:42 +0600

    xx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import Header from "./components/Header";
-
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth";
+import Blogs from "./components/Blogs";
+import AddBlog from "./components/AddBlog";
+import BlogDetail from "./components/BlogDetail";
+import UserBlogs from "./components/UserBlogs";
 
 function App() {
   return (
-  <Header />
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/myBlogs" element={<UserBlogs />} />
+          <Route path="/myBlogs/:id" element={<BlogDetail />} />
+          <Route path="/blogs/add" element={<AddBlog />} />
+        </Routes>
+      </main>
+    </React.Fragment>
   );
 }
 
